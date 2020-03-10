3420276fd6dae2bb522a9a7dd5ed45ad4ae10d48
Patrick S <patrickceg@outlook.com>
2020-03-10 19:32:27 -0400

    Add a Spring template

---
+++ wstg_fmtstring/fmtstringvuln/src/main/java/org/owasp/wstg/fmtstringvuln/FmtstringvulnApplication.java
@@ -0,0 +1,13 @@
+package org.owasp.wstg.fmtstringvuln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmtstringvulnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmtstringvulnApplication.class, args);
+	}
+
+}
---
+++ wstg_fmtstring/fmtstringvuln/src/test/java/org/owasp/wstg/fmtstringvuln/FmtstringvulnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.owasp.wstg.fmtstringvuln;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmtstringvulnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
